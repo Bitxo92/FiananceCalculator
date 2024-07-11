3df298b78bc4b2e70a3c6718faae7f3c0d43af5e
Alejandro Patiño García <alexpatino92@yahoo.com>
2024-07-11 18:18:15 +0200

    added image icon to app

--- src/application/FinanceCalculator.java
+++ src/application/FinanceCalculator.java
@@ -1,34 +1,43 @@
-package application;import javafx.application.Application;
+package application;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class FinanceCalculator extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("finance_calculator.fxml"));
-        primaryStage.setTitle("Calculadora Financiera");
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("finance_calculator.fxml"));
+		primaryStage.setTitle("Calculadora Financiera");
 
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		// Load icon from resources folder
+		Image icon = new Image(getClass().getResourceAsStream("icon.png"));
 
-        primaryStage.setScene(scene);
-        
-        // Establecer el tamaño fijo de la ventana
-        primaryStage.setResizable(false);
-        
-        // Opcional: Establecer tamaño mínimo y máximo si es necesario
-         primaryStage.setMinWidth(500);
-         primaryStage.setMinHeight(400);
-         primaryStage.setMaxWidth(500);
-         primaryStage.setMaxHeight(400);
+		// Set the application icon
+		primaryStage.getIcons().add(icon);
 
-        primaryStage.show();
-    }
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+		primaryStage.setScene(scene);
+
+		// Establecer el tamaño fijo de la ventana
+		primaryStage.setResizable(false);
+
+		// Opcional: Establecer tamaño mínimo y máximo si es necesario
+		primaryStage.setMinWidth(500);
+		primaryStage.setMinHeight(400);
+		primaryStage.setMaxWidth(500);
+		primaryStage.setMaxHeight(400);
+
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
